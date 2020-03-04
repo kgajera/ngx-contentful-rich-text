f979e079983f67688f3c3da9360aa9c27a1c0528
Kishan Gajera <me@kgajera.com>
2020-03-03 19:59:01 -0500

    Remove unneeded conditional

--- projects/ngx-contentful-rich-text/src/lib/directives/mark-renderer-host.directive.ts
+++ projects/ngx-contentful-rich-text/src/lib/directives/mark-renderer-host.directive.ts
@@ -26,13 +26,12 @@ export class MarkRendererHostDirective implements OnInit {
 
   ngOnInit() {
     let component: Type<MarkRenderer | TextValueComponent>;
-    const numMarks: number = this.node.marks.length;
     if (isNaN(this.node.markIndex)) {
       this.node.markIndex = 0;
     } else {
       this.node.markIndex += 1;
     }
-    if (numMarks && numMarks > this.node.markIndex) {
+    if (this.node.marks.length > this.node.markIndex) {
       const mark: Mark = this.node.marks[this.node.markIndex];
       component = this.rendererProvider.getMarkRenderer(mark.type);
     } else {
